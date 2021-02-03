55316a13e2c088844a300622322612427862c9dc
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-02-02 16:14:04 -0800

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -9,8 +9,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'eval-source-map',
-  devServer: {
-    contentBase: './dist'
+  devServer: {               
+    contentBase: './dist'    
   },
   plugins: [
     new CleanWebpackPlugin(),
