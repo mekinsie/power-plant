89e9050e7d564b4fd01614e0edd7a62d7c1d61a1
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-13 11:08:42 -0700

    create change and store state functions amd function factory
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

---
+++ src/plants.js
@@ -0,0 +1,35 @@
+
+module.exports = { stateControl, changeState, feed, blueFood, hydrate, superWater};
+
+// This function stores our state.
+
+const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+const stateControl = storeState();
+
+// This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  }
+}
+
+// We create four functions using our function factory. We could easily create many more.
+
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
