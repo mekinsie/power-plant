fa169448a3f92a78a2df540218c2c0ec446b9443
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-04-13 11:41:03 -0700

    test changestange with hydrate and supawata
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>

--- __tests__/plants.tests.js
+++ __tests__/plants.tests.js
@@ -5,7 +5,6 @@ describe('changeState', () => {
   test('should increase property value', () => {
     let plant = {};
     expect(changeState("soil")(5)(plant)).toEqual({ soil: 5 });
-
   });
   test('it should feed the plant by increasing its soil by 1', () => {
     let plant = {};
@@ -14,6 +13,14 @@ describe('changeState', () => {
   test('it should increase soil by 5', () => {
     let plant = {};
     expect(blueFood(plant)).toEqual({ soil: 5 });
+  });
+  test('it should increase water by 1', () => {
+    let plant = {};
+    expect(hydrate(plant)).toEqual({ water: 1 });
+  });
+  test('it should increase water by 5', () => {
+    let plant = {};
+    expect(superWater(plant)).toEqual({ water: 5 });
   });
 
 
